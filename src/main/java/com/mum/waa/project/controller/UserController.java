27a2efdf58df49c67d91c2f05cc8f1012ab18943
Rajan Prasad Upadhyay <rajanpupa@gmail.com>
2015-06-16 16:26:58 -1000

    bidding done, ajax fixed

--- src/main/java/com/mum/waa/project/controller/UserController.java
+++ src/main/java/com/mum/waa/project/controller/UserController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mum.waa.project.domain.Auction;
 import com.mum.waa.project.domain.Bid;
+import com.mum.waa.project.domain.Message;
 import com.mum.waa.project.service.AuctionService;
 
 @Controller
@@ -17,15 +19,41 @@ public class UserController {
 	@Autowired
 	AuctionService auctionService;
 	
-	@RequestMapping(value="/makebid", method=RequestMethod.PUT)
-	public @ResponseBody Auction makeBid(Model model, Bid bid){
+	@RequestMapping(value="/makebidtest",produces="application/json", method=RequestMethod.GET)
+	public @ResponseBody Message makeBid(){
+		Auction auction = auctionService.getAuctionById("P0001");
 		
-		return auctionService.bid(bid.getAuctionId(), bid.getBidAmount());
+		Message message = new Message();
+		
+		if(auction==null){
+			message.setPass(false);
+		}else{
+			message.setPass(true);
+		}
+		return message;
+	}
+	
+	@RequestMapping(value="/makebid", method=RequestMethod.POST)
+	public @ResponseBody Message makeBid(@RequestBody Bid bid){
+		String id = bid.getAuctionId();
+		Double amount = bid.getBidAmount();
+		
+		System.out.println("id=" + id + " amount=" +amount);
+		Auction auction = auctionService.bid(id, amount);
+		Message message = new Message();
+		if(auction == null){
+			message.setPass(false);
+			message.setMessage("Bidding Failed");
+		}else{
+			message.setPass(true);
+			message.setMessage("Bidding succeed");
+			message.setResult(auction.getMaxBidAmount().toString());
+		}
+		return message;
 	}
 	
 	@RequestMapping("/createAuction")
 	public String createAuction(@ModelAttribute("auction") Auction auction,Model model) {
-
 		//model.addAttribute("auctions", auctionService.getAllAuctions());
 		return "user/createAuction";
 	}
---
+++ src/main/java/com/mum/waa/project/domain/Message.java
@@ -0,0 +1,35 @@
+package com.mum.waa.project.domain;
+
+public class Message {
+	
+	Boolean pass=true;
+	String message;
+	String result;
+
+	
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public Boolean getPass() {
+		return pass;
+	}
+
+	public void setPass(Boolean pass) {
+		this.pass = pass;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
